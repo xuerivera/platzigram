6130f80af23d99e8131af5d4fe043465bb4bb229
xuerivera <robert.rivera@outlook.com>
2022-01-18 15:42:28 -0700

    create post
    add post form and create post func

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 #django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+#forms
+from posts.forms import PostForm
+
 #misc
 from datetime import datetime
+
+# Forms
+from posts.forms import PostForm
 
 # Create your views here.
 posts = [
@@ -48,3 +55,26 @@ posts = [
 def list_posts(request):
     """List existing posts"""
     return render(request, 'post/feed.html', {'posts': posts})
+
+
+@login_required
+def create_post(request):
+    """Create new post view."""
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+
+    else:
+        form = PostForm()
+
+    return render(
+        request=request,
+        template_name='post/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
