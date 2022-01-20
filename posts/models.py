290b9191fb076af5fb52ed4a5eebbc986c3409e2
xuerivera <robert.rivera@outlook.com>
2022-01-19 19:17:18 -0700

    fix profile pic
    fixed profile pic and added  link to singup or login

--- posts/models.py
+++ posts/models.py
@@ -1,18 +1,22 @@
-"""posts models"""
+"""Posts models."""
 
-#django
+# Django
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
-    """Post model"""
+    """Post model."""
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
     profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='posts/photos')
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """Return title and username"""
-        return '@{} - {}'.format(self.user.username, self.title)
+        """Return title and username."""
+        return '{} by @{}'.format(self.title, self.user.username)
--- posts/views.py
+++ posts/views.py
@@ -1,59 +1,18 @@
 #django
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
 #forms
 from posts.forms import PostForm
-
-#misc
-from datetime import datetime
-
-# Forms
-from posts.forms import PostForm
+# models
+from posts.models import Post
 
 # Create your views here.
-posts = [
-    {
-        'title': 'Mont Blanc',
-        'user': {
-            'name': 'Yésica Cortés',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600?image=1036',
-    },
-    {
-        'title': 'Via Láctea',
-        'user': {
-            'name': 'Christian Van der Henst',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/800/?image=903',
-    },
-    {
-        'title': 'Nuevo auditorio',
-        'user': {
-            'name': 'Uriel (thespianartist)',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=1076',
-    },
-    {
-        'title': 'Statue of Liberty',
-        'user': {
-            'name': 'Elon Musk',
-            'picture': 'https://picsum.photos/60/60/?image=1015'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=903',
-    },
-]
+
 
 @login_required
 def list_posts(request):
     """List existing posts"""
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'post/feed.html', {'posts': posts})
 
 
